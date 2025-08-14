5c43602a086aba00903016f699e565e4b374d723
Biwlw <147769730+Biwlw@users.noreply.github.com>
2025-08-14 12:08:01 +0300

    Add gunicorn to requirements

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 set -o errexit
 
-pip install -r requirements.txt
+# Добавьте очистку кэша
+pip cache purge
+
+# Установка с игнорированием установленных пакетов
+pip install --ignore-installed -r requirements.txt
 
 python manage.py collectstatic --noinput
 python manage.py migrate
